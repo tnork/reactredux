e58b864c1e950615abdc6c7b4c196c32997ef8af
unknown <tylerdnorkus@gmail.com>
2017-09-01 20:00:07 -0700

    ''

--- app.js
+++ app.js
@@ -32,10 +32,10 @@ app.use(function(req, res, next){
   next();
 });
 
-app.use(logger('dev'));  // Dev Testing
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(logger('dev'));  // Dev Testing
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
 
 //Routes
 app.get('/', function(req, res, next){
@@ -49,23 +49,23 @@ app.get('/', function(req, res, next){
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-
-  // res.render('error');
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+//
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//
+//   // res.render('error');
+// });
 
 app.listen(app.get('port'), function() {
   console.log('Server active on port ' + app.get('port'));
