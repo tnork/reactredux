f91581aee73ffdfc12f1e78fc247effb4c7590fd
unknown <tylerdnorkus@gmail.com>
2017-09-01 19:30:16 -0700

    'Improvement'

--- app.js
+++ app.js
@@ -8,7 +8,26 @@ app.set('port', process.env.Port || 5000);
 
 // Route Handlers (APP: GET, POST, PUT, DELETE, PATCH)
 app.get('/', function(req, res) {
-  res.send({'Hi there'});
+  res.send('Hi there');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+
+  res.render('error');
 });
 
 app.listen(app.get('port'), function() {
