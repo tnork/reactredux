bb3c987db46afaa95e39f63ad2c12a2354646194
unknown <tylerdnorkus@gmail.com>
2017-09-04 17:15:37 -0700

    ''

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 
 const keys = require('./config/keys');
-require('./models/User'); // Have to require data models to declare before services using those
+require('./models/user'); // Have to require data models to declare before services use
 require('./services/passport');
 const authRoutes = require('./routes/authRoutes');
 
